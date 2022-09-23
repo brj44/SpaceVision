f2e5c205d836338e7a23997d232e6659fdd86d84
Benito Juarez <brj44@txstate.edu>
2022-09-22 21:22:46 -0500

    Created the base React web application and removed a lot of base react fluff

---
+++ space-vision/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo from './SpaceVisionLogoV1.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Space Vision Homepage</h1>
+          <img src={logo} alt="Logo"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
