36151de7725463e7a8dc8dd9bdfcc46d7bfc92a1
Benito Juarez <brj44@txstate.edu>
2022-09-23 21:15:23 -0500

    Created a base api call template
    Created the coronal mass ejection api call function and displayed the latitude data on the home page

---
+++ space-vision/src/APIs/coronalMassEjectionAPICall.js
@@ -0,0 +1,19 @@
+/*
+    This is a basic API call using the built-in fetch functionality.
+    the two key parts are the fetch function and then the await response.json() function.
+    the fetch function hits the url given to it and then returns the data from the url into
+    the response variable. Then await waits for all the data to be transferred before inputing
+    the data into the variable. response.json(); inputs the data from the url into the
+    coronalMassEjectionData variable in a json format, so we can easily access the data later on.
+    Some of this info might be slightly wrong and this is a super high level view of it so
+    please go research javascript promises and different ways of calling APIs in javascript.
+ */
+
+async function coronalMassEjectionAPICall() {
+    let APIKey = '8gM8MK39JmyKbq0S9WJyhD2pagekamrppBVDAiNM';
+    let response = await fetch('https://api.nasa.gov/DONKI/CMEAnalysis?startDate=2016-09-01&endDate=2016-09-30&mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key='+ APIKey);
+    let coronalMassEjectionData = await response.json();
+    return coronalMassEjectionData;
+}
+
+export  default  coronalMassEjectionAPICall;
--- space-vision/src/App.js
+++ space-vision/src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
 import logo from './SpaceVisionLogoV1.png';
+import coronalMassEjectionAPICall from "./APIs/coronalMassEjectionAPICall";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Space Vision Homepage</h1>
-          <img src={logo} alt="Logo"/>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    /*
+        This function calls our API call function from the other file.
+        It requires await because the function will only return a promise
+        at first when we need the real data from the api call
+     */
+    const fetchData = async () => {
+        setData(await coronalMassEjectionAPICall());
+    }
+
+    //useEffect is called everytime the page is updated I believe.
+    useEffect(() => {
+        fetchData().then(r => {
+            console.log(r);
+        });
+    }, []);
+
+    // This function converts our json into a map and only
+    // converts the latitude data into a paragraph html element.
+    const tableRows = data.map((element) => {
+        return(
+            <p key = {element.latitude}>{element.latitude}</p>
+        )
+    })
+
+    //This is the main "meat" of a react file. This is the component that gets rendered.
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Space Vision Homepage</h1>
+                <div>{tableRows}</div>
+                <img src={logo} alt="Logo"/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
