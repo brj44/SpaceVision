9c8eb4fab0ea289a8806709e0321fe0080870654
Matvey Lubaev <mel207@txstate.edu>
2022-09-30 13:46:37 -0500

    added research code

---
+++ research/matveyresearch.js
@@ -0,0 +1,9 @@
+console.log('javascript is goin')
+var number =10;
+var string='bruother';
+var isNeat=true;
+
+document.getElementById('blob').innerHTML = string;
+document.getElementById('blob').addEventListener('click', function(){
+    alert('I got clicked');
+});
