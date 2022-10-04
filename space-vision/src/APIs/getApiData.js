b65c8f3339dcec4029de4b5a2b2fcaf3c3050a0e
Matvey Lubaev <mel207@txstate.edu>
2022-10-03 22:12:11 -0500

    adding getApiData.js

---
+++ space-vision/src/APIs/getApiData.js
@@ -0,0 +1,21 @@
+
+import { createJson } from './bru.js';
+
+
+var x = (await createJson('apod'));   
+console.log(x.url);  //to log all the info instead, do 'console.log(x)' ;to log the published date, log 'x.date', etc.
+
+var x = await createJson('asteroid neo');
+console.log(x.near_earth_objects);
+
+var x = await createJson('insight');
+console.log(x);
+
+var x = await createJson('mars rover photos');
+console.log(x);    //example of logging photo to console instead, 'console.log(x.photos[0].img_src)'
+                   //example of logging photo id to console instead, 'console.log(x.photos[0].id)'
+
+
+//earth  API call wont work
+//var x = await createJson('earth');
+//console.log(x);
