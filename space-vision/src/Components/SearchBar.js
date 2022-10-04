7eed1eb8740a23099281b958c185006e128e8f61
Rayyan Khan <r_k150@txstate.edu>
2022-10-03 21:37:03 -0500

    CSS file and UI updates

--- space-vision/src/Components/SearchBar.js
+++ space-vision/src/Components/SearchBar.js
@@ -1,12 +1,18 @@
+import './SearchBar.css'
+
 const SearchBar = () =>(
     <form action="/" method="get">
-        <input
+        <label htmlFor="header-search">
+           <span className="Search-Bar">Enter Query</span> 
+        </label>
+        <input className='Search-Input-Box'
             type="text"
             id="header-search"
+        
             placeholder="Search NASA API "
             name="s" 
         />
-        <button type="submit">Search</button>
+        <button className='Search-Button' type="submit">Search</button>
     </form>
 );
 
