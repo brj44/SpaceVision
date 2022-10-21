01053b29c522b3041a57610f09baae5b7e2de942
Matvey Lubaev <mel207@txstate.edu>
2022-10-20 20:38:01 -0500

    removed accessNasaAPI from Mars

--- space-vision/src/APIs/MarsData.js
+++ space-vision/src/APIs/MarsData.js
@@ -1,8 +1,15 @@
-import createJson from "./accessNasaAPI";
+
 
 async function getPhoto(){
+    let path = '/mars-photos/api/v1/rovers/curiosity/photos?';
+    let earth_date= 'earth_date=2022-10-17&'     //use format:'earth_date=[date]&'
+    let camera= ''           //use format: camera=[string]&
+    let page = ''            //use format: page=[int]&
+    let api_call=path + earth_date + camera + page;
+
     const APIKey = process.env.REACT_APP_API_KEY;
-    let data = await createJson('mars rover photos');
+    let response = await fetch('https://api.nasa.gov' + api_call + 'api_key=' + APIKey );
+    let data = await response.json();
     return data;
 }
 
