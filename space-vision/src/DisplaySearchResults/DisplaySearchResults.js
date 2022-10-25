18abbe5b26dd16f2f6c73bc109e8eea5974706a6
Rayyan Khan <r_k150@txstate.edu>
2022-10-25 17:51:32 -0500

    SV-65
    SV-65 Reformatting page to prepare for api data

--- space-vision/src/DisplaySearchResults/DisplaySearchResults.js
+++ space-vision/src/DisplaySearchResults/DisplaySearchResults.js
@@ -1,44 +1,26 @@
 import './DisplaySearchResults.css'
 function SearchResults(){
+let dummy_search_result = [
+{title: "Today in Space",
+ DOA: "Two very different payloads flying on one rocket are ready for their ride to space. Creating an integrated stack that stands approximately 27 feet tall, the National Oceanic and Atmospheric Administration’s (NOAA) Joint Polar Satellite System-2 (JPSS-2) and NASA’s Low-Earth Orbit Flight Test of an Inflatable Decelerator (LOFTID) spacecraft are safely secured inside the United Launch Alliance (ULA) Atlas V rocket’s payload fairing at Vandenberg Space Force Base in California."
 
-
+},
+{
+    title: "Tomorrow in space...",
+    DOA: "9/29/22"
+}]
+const displayResults=()=>{
+    let results = []
+        for(var i = 0; i < dummy_search_result.length; i++){
+            results.push(<header className="header-info">{dummy_search_result[i].title}</header>)
+            results.push(<p className="search-info">{dummy_search_result[i].DOA}</p>)
+        }
+        return results
+}
     return(
-        <>
-            <h1>Displaying Search Results</h1>
-            <textarea className = "Display-Info" rows= "45" cols = "90">
-                Display search here ... Testing with dummy data --------------------------------------------------------------------------------------------
-
-
-
-
-                Result 1:  NASA’s James Webb Space Telescope has captured a lush, highly detailed landscape – the iconic Pillars of Creation – where new stars are forming within dense clouds of gas and dust. The three-dimensional pillars look like majestic rock formations, but are far more permeable. These columns are made up of cool interstellar gas and dust that appear – at times – semi-transparent in near-infrared light.
-
-                 Webb’s new view of the Pillars of Creation, which were first made famous when imaged by NASA’s Hubble Space Telescope in 1995, will help researchers revamp their models of star formation by identifying far more precise counts of newly formed stars, along with the quantities of gas and dust in the region. Over time, they will begin to build a clearer understanding of how stars form and burst out of these dusty clouds over millions of years.
-
-                 --------------------------------------------------------------------------------------------
-
-
-                 Result 2: Astronomers looking into the early universe have made a surprising discovery using NASA’s James Webb Space Telescope: a cluster of massive galaxies in the process of forming around an extremely red quasar. The result will expand our understanding of how galaxy clusters in the early universe came together and formed the cosmic web we see today.
-
-                A quasar, a special type of active galactic nucleus (AGN), is a compact region with a supermassive black hole at the center of a galaxy. Gas falling into a supermassive black hole makes the quasar bright enough to outshine all the galaxy’s stars.
-
-                --------------------------------------------------------------------------------------------
-
-
-                Result 3: NASA will host a media teleconference at 11 a.m. EDT on Tuesday, Oct. 25, to discuss the next science investigations, technology demonstrations, crew supplies, and hardware bound for the International Space Station aboard Northrop Grumman’s Cygnus spacecraft on the company’s 18th commercial resupply mission for NASA.
-
-                    Audio of the media call will stream live at
-                
-                    --------------------------------------------------------------------------------------------
-
-
-                     Result 4:
-                        Two very different payloads flying on one rocket are ready for their ride to space. Creating an integrated stack that stands approximately 27 feet tall, the National Oceanic and Atmospheric Administration’s (NOAA) Joint Polar Satellite System-2 (JPSS-2) and NASA’s Low-Earth Orbit Flight Test of an Inflatable Decelerator (LOFTID) spacecraft are safely secured inside the United Launch Alliance (ULA) Atlas V rocket’s payload fairing at Vandenberg Space Force Base in California.
-
-                    The primary payload, JPSS-2, is the third satellite in the Joint Polar System series – a collaborative program between NOAA and NASA – and will be one of five satellites to capture data and improve weather forecasts, leading scientists to better predict for extreme weather events and climate change.
-                    
-            </textarea>
-     </>       
+        <div>
+            {displayResults()}
+        </div>
     )
 }
 
