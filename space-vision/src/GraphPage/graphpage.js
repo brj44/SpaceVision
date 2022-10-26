36550fa408ce6b7dadb78ddb15fe338e0aed0e8b
Tara Meazell <tgm56@txstate.edu>
2022-10-26 14:18:49 -0500

    try to display raw data

--- space-vision/src/GraphPage/graphpage.js
+++ space-vision/src/GraphPage/graphpage.js
@@ -1,13 +1,44 @@
-function Graphpage(){
+import CADAPICall from "../APIs/CADAPICall";
+import {useEffect, useState} from "react";
 
+function Graphpage(){
+    const [cadData, setCadData] = useState({data:[]});
+    const [designation, setDesignation] = useState("");
+
+    const graphData = [];
+
+    const fetchData = async (designation) => {
+        if (designation === "") {
+            setCadData(await CADAPICall());
+        }else {
+            setCadData(await CADAPICall(designation));
+        }
+    }
+
+    useEffect( () => {
+        fetchData(designation).then(r => {
+            console.log(r);
+        });
+    }, []);
+
+    cadData ? console.log(cadData.data[0]) : fetchData();
 
     return(
-            <>
-                <h1>Graph Page</h1>
-                
-            </>
+        <>
+            <h1>Graph Page</h1>
+            {
+                cadData.data ? cadData.data.map((cad) => {
+                    return (
+                        <div key = {cad[0]}>
+                            <p>Distance: {cad[4]} au</p>
+                            <p>Diameter: {cad[11]} km</p>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 
 }
 
-export default Graphpage;
+export default GraphPage;
