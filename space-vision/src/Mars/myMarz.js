ed49f9509cbdf26239e773982c91d61db3a866ce
Matvey Lubaev <mel207@txstate.edu>
2022-10-27 22:09:36 -0500

    rover selection

--- space-vision/src/Mars/myMarz.js
+++ space-vision/src/Mars/myMarz.js
@@ -5,17 +5,20 @@ import {useEffect, useState} from "react";
 export default function Marz(){
 
     const [date, setDate] = useState("");  
+    const [rover, setRover] = useState("curiosity");  
     const [click, setClick] = useState(false);
     const [data, setData] = useState({photos:[]});
+    const [firstCall,setFirstCall] = useState(false);
 
-    const fetchData = async (date) => {
+    const fetchData = async (date,rover) => {
         setClick(true)
         if (click) {
-            if (date === '' ) {
-                setData(await GetPhoto());
+            if (date === ''){                                  
+                setData(await GetPhoto(null,rover));
             } else {
-                setData(await GetPhoto(date));
+                setData(await GetPhoto(date,rover));
             }
+            setFirstCall(true);
         }
     }
 
@@ -25,15 +28,21 @@ export default function Marz(){
         console.log(click)
         }
 
+    const getRover = (e) => {
+        setRover(e.target.value);
+        console.log(e.target.value)
+        console.log(click)
+        }
+
     const clicked = () => {
-        fetchData(date).then(r => {
+        fetchData(date,rover).then(r => {
             console.log(r)
             console.log(date);
         });
     }
 
     useEffect( () => {        
-        fetchData(date).then(r => {
+        fetchData(date,rover).then(r => {
             console.log(r);
         });
     }, []);
@@ -42,16 +51,28 @@ export default function Marz(){
         <>
             <h1>Mars</h1>
             {
-            <div className="EnterDate">
-                ENTER DATE: 
-            <input className="TextInput" onChange={getDate} placeholder=" YYYY-MM-DD "/>
-            <button className="Button" onClick={clicked}>Search</button>
-            <p>(Minumim 2012-08-06)</p>
+            <div className="EnterDate">ENTER DATE: 
+                <input className="TextInput" onChange={getDate} placeholder=" YYYY-MM-DD "/>
+                <button className="Button" onClick={clicked}>Search</button>
             </div>
             } 
+
             {
-                data.photos ? data.photos.map((x) => {
-                    return (                       
+            <div className="rover">
+                <div className="SelectRover">Select Rover:
+                    <label className='block'> <input type="radio" name="rover" value="curiosity" id="curiosity" defaultChecked onChange={getRover}   /> Curiosity <div className="Range">Range: 2012-08-06  to  today</div></label>
+                    <label className='block' ><input type="radio" name="rover" value="opportunity" id="opportunity"  onChange={getRover}/> Opportunity<div className="Range">Range: 2004-01-26 to 2018-06-11</div></label>
+                    <label className='block' ><input type="radio" name="rover" value="spirit" id="spirit" onChange={getRover} /> Spirit<div className="Range">Range: 2004-01-05 to 2010-03-21</div></label>
+                </div>
+            </div>
+            
+            }
+
+            {
+                firstCall&&data.photos.length==0?<p>No images found</p>:
+
+                data.photos? data.photos.map((x) => {
+                    return (                     
                         <div key = {x.id}>
                             <p>id: {x.id}, earth date: {x.earth_date}, rover: {x.rover.name}, camera: {x.camera.full_name}</p>
                             <img
@@ -59,9 +80,11 @@ export default function Marz(){
                             />
                             <p>---------------------------------------------------------</p>
                         </div>
+                    
                     )
                 }) : <h1>Error</h1>
             }
+        
         </>
     )
 }
