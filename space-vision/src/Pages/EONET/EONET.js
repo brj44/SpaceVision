35073371732f0c962acf35b3188e7c878f92cb5e
Matvey Lubaev <mel207@txstate.edu>
2022-11-13 01:17:35 -0600

    format natural events

--- space-vision/src/Pages/EONET/EONET.js
+++ space-vision/src/Pages/EONET/EONET.js
@@ -1,5 +1,20 @@
 import GetEvents from "../../APIs/EONET_API";
 import {useEffect, useState} from "react";
+import "./EONET.css"
+import fire from "../../images/fiyer.png"
+import drought from "../../images/drouht.png";
+import dust from "../../images/dust.png";
+import storm from "../../images/dees.png";
+import earthquake from "../../images/eq.png";
+import flood from "../../images/flood.png";
+import landslide from "../../images/landslide.png";
+import manmade from "../../images/manmade.png";
+import seaLakeIce from "../../images/seaLakeIce.png";
+import snow from "../../images/snow.png";
+import tempExtremes from "../../images/tempExtremes.png";
+import volcanoes from "../../images/volcanoes.png";
+import waterColor from "../../images/waterColor.png";
+
 
 function EONET(){
  
@@ -7,7 +22,7 @@ function EONET(){
     const [click, setClick] = useState(true);
     const [data, setData] = useState({events:[]});
     const [firstCall,setFirstCall] = useState(false);
-
+ 
     const fetchData = async (eventType) => {
         setClick(true)
         if (click) {                                 
@@ -34,54 +49,95 @@ function EONET(){
         });
     }, []);
 
+    const picType = (x) => {
+        if (x === "severeStorms"){
+            return (<img className="storm" src={storm} alt="stormPic"/>)
+        }
+        if (x === "wildfires"){
+            return (<img className="fire" src={fire} alt="firePic"/>)
+        }
+        if (x === "drought"){
+            return (<img className="drought" src={drought} alt="droughtPic"/>)
+        }
+        if (x === "dustHaze"){
+            return (<img className="dust" src={dust} alt="dustHazePic"/>)
+        }
+        if (x === "earthquakes"){
+            return (<img className="earthquake" src={earthquake} alt="EQPic"/>)
+        }
+        if (x === "floods"){
+            return (<img className="floods" src={flood} alt="floodPic"/>)
+        }
+        if (x === "landslides"){
+            return (<img className="landslides" src={landslide} alt="LSPic"/>)
+        }
+        if (x === "manmade"){
+            return (<img className="manmade" src={manmade} alt="MMPic"/>)
+        }
+        if (x === "seaLakeIce"){
+            return (<img className="seaLakeIce" src={seaLakeIce} alt="SLIPic"/>)
+        }
+        if (x === "snow"){
+            return (<img className="snow" src={snow} alt="snowPic"/>)
+        }
+        if (x === "tempExtremes"){
+            return (<img className="tempExtremes" src={tempExtremes} alt="tempExtremePic"/>)
+        }
+        if (x === "volcanoes"){
+            return (<img className="volcanoes" src={volcanoes} alt="volcanoPic"/>)
+        }
+        if (x === "waterColor"){
+            return (<img className="waterColor" src={waterColor} alt="WCPic"/>)
+        }
+    }
+
     return(
         <>
-            <h1>EONET</h1>
+            <h1>Natural Disasters</h1>
             {
-            <div className="EnterDate">    
-                <button className="Button" onClick={clicked}>Load</button>
+            <div className="bundle"> 
+                <button className="Button" onClick={clicked}>Load</button>  
+                <div className="dropdown">
+                    <select className="select" value={eventType} onChange={getEventType}>
+                        <option value="">All Events</option>
+                        <option value="drought">Drought</option>
+                        <option value="dustHaze">DustHaze</option>
+                        <option value="earthquakes">Earthquakes</option>
+                        <option value="floods">Floods</option>
+                        <option value="landslides">Landslides</option>
+                        <option value="manmade">Manmade</option>
+                        <option value="seaLakeIce">SeaLakeIce</option>
+                        <option value="severeStorms">SevereStorms</option>
+                        <option value="snow">Snow</option>
+                        <option value="tempExtremes">TempExtremes</option>
+                        <option value="volcanoes">Volcanoes</option>
+                        <option value="waterColor">WaterColor</option>
+                        <option value="wildfires">Wildfires</option>
+                    </select>
+                </div>
             </div>
-            } 
-
-            {
-            <div>
-            <select value={eventType} onChange={getEventType}>
-                <option value="">All Events</option>
-                <option value="drought">Drought</option>
-                <option value="dustHaze">DustHaze</option>
-                <option value="earthquakes">Earthquakes</option>
-                <option value="floods">Floods</option>
-                <option value="landslides">Landslides</option>
-                <option value="manmade">Manmade</option>
-                <option value="seaLakeIce">SeaLakeIce</option>
-                <option value="severeStorms">SevereStorms</option>
-                <option value="snow">Snow</option>
-                <option value="tempExtremes">TempExtremes</option>
-                <option value="volcanoes">Volcanoes</option>
-                <option value="waterColor">WaterColor</option>
-                <option value="wildfires">Wildfires</option>
-            </select>
-            </div>
-            
-                    
-
             }
-
+        
             {
             firstCall&&data.events.length===0?<p>No results available</p>:
-
+            
             data.events? data.events.map((x) => {
+                
                 return (                     
-                    <div key = {x.id}>
-                        <p>id: 
-                            {x.id}, 
-                            title: {x.title},
-                            date: {x.geometry[0].date.slice(0,10)},
-                            location: {x.geometry[0].coordinates[0]},
-                                      {x.geometry[0].coordinates[1]}
-                            <a href={x.sources[0].url}> Learn More</a>
-                        </p>
-                        <p>. . . . . . . . . . . . . . . . . . . . . . .</p>
+                    <div className="box" key = {x.id}>
+                        
+                        {picType(x.categories[0].id)}
+                        <div className={((x.categories[0].id).toString())+"Box"}>
+                            <p className ="title">{x.title}</p>
+                            <p className = "date">{x.geometry[0].date.slice(0,10)}</p>
+                            <p className = "location">Coordinates: {x.geometry[0].coordinates.length===2?Math.round((x.geometry[0].coordinates[0]) * 100) / 100:Math.round(x.geometry[0].coordinates[0][0][0] * 100) / 100}           
+                                                                    , {x.geometry[0].coordinates.length===2?Math.round((x.geometry[0].coordinates[1]) * 100) / 100:Math.round(x.geometry[0].coordinates[0][0][1] * 100) / 100}                                            
+                                            {x.sources.length===0?
+                                                console.log("no url")
+                                            :<a className="link" href={x.sources[0].url}>Learn More</a>}
+                            </p>
+                        </div>
+                        
                     </div>
                 )
             }) : <h1>Error</h1>
