d0d38c0c2f7a2f557ce1c8a97de08e716088375b
Tara Meazell <tgm56@txstate.edu>
2022-11-14 09:48:15 -0600

    fixed p1 console error

--- space-vision/src/Pages/APOD/apod.js
+++ space-vision/src/Pages/APOD/apod.js
@@ -56,7 +56,7 @@ function Apod(){
                 <p> {photo.explanation} </p>
 
                 <h2> Want to see a different day? enter it below </h2>
-                <p1> Must be a date in the past </p1>
+                <p> Must be a date in the past </p>
                 <TextField
                     label="date"
                     id="date"
