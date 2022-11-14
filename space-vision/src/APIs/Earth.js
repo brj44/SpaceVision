18e663007adf18776c15adb44f2c2ce1aa768cab
Tara Meazell <tgm56@txstate.edu>
2022-11-14 14:36:13 -0600

    Added call to earth api on earthpage

--- space-vision/src/APIs/Earth.js
+++ space-vision/src/APIs/Earth.js
@@ -1,18 +1,19 @@
-async function earthAPICall(longitude, latitude, date){
+async function EarthAPICall(longitude, latitude, date){
+    const APIKey = process.env.REACT_APP_API_KEY;
     let response;
-    if (longitude === undefined || latitude === undefined)
+    if (longitude === '' || latitude === '')
     {
-        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=100.75&lat=1.5&api_key=REACT_APP_API_KEY');
+        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=29.8833&lat=97.9414&api_key=' + APIKey);
     }
     else if (date === undefined)
     {
-        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=' + longitude + '&lat=' + latitude + '&api_key=REACT_APP_API_KEY');
+        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=' + longitude + '&lat=' + latitude + '&api_key=' + APIKey);
     }
     else
     {
-        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=' + longitude + '&lat=' + latitude + '&date=' + date + 'api_key=REACT_APP_API_KEY');
+        response = await fetch('https://api.nasa.gov/planetary/earth/imagery?lon=' + longitude + '&lat=' + latitude + '&date=' + date + 'api_key=' + APIKey);
     }
     return await response.json();
 }
 
-export default earthAPICall;
+export default EarthAPICall;
