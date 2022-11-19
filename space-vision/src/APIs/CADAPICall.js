d2620c41ae4008ba48d1ce8525c7ca7d948075c2
Benito Juarez <brj44@txstate.edu>
2022-11-18 23:57:48 -0600

    SV-102 Added bypass for cors issue
    SV-102 Added bypass for cors issue for some apis

--- space-vision/src/APIs/CADAPICall.js
+++ space-vision/src/APIs/CADAPICall.js
@@ -1,12 +1,13 @@
 async function CADAPICall(designation){
     let response;
+    const corsBypass = 'https://corsbypass.fly.dev/';
     if (designation === undefined)
     {
-        response = await fetch('https://ssd-api.jpl.nasa.gov/cad.api?diameter=true&limit=20');
+        response = await fetch(corsBypass + 'https://ssd-api.jpl.nasa.gov/cad.api?diameter=true&limit=20');
     }
     else
     {
-        response = await fetch('https://ssd-api.jpl.nasa.gov/cad.api?body=' + designation + '&diameter=true&limit=20');
+        response = await fetch(corsBypass + 'https://ssd-api.jpl.nasa.gov/cad.api?body=' + designation + '&diameter=true&limit=20');
     }
     return await response.json();
 }
