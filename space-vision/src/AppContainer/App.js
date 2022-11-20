1aae0ce5b143100e8bd4a02a6bf5a0ebc76c3476
Rayyan Khan <r_k150@txstate.edu>
2022-11-19 20:45:44 -0600

    SV-52 Readded line of code to maintain search bar functionality

--- space-vision/src/AppContainer/App.js
+++ space-vision/src/AppContainer/App.js
@@ -129,6 +129,9 @@ function App() {
                         <Header
                             setPage = {setPage}
                         />
+                         <SearchResults 
+                            searchKey = {query}
+                        />
                         <SearchBar
                             setPage = {setPage}
                             setQuery = {setQuery}
