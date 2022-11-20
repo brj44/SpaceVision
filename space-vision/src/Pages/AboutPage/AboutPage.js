8b504161dc65b983989ed31003a604b44b476aaf
Rayyan Khan <r_k150@txstate.edu>
2022-11-19 20:20:47 -0600

    Added draft of about page

--- space-vision/src/Pages/AboutPage/AboutPage.js
+++ space-vision/src/Pages/AboutPage/AboutPage.js
@@ -1,10 +1,17 @@
+import './AboutPage.css'
 function AboutPage(){
+
 
 
     return(
         <>
-            <h1>About SpaceVision</h1>
+            <h1 className="header">About SpaceVision</h1>
+            <div className="About">SpaceVision was made to allow users to access data from NASA API's without having to generate their own key.
+            Users can search the NASA Photo and Video library by keywords using the search bar, and can also see the picture of the day from NASA on the APOD page. There are still more feautures available for use.
+            SpaceVision was created in 2022 as a team project for the semester.
+            </div>
             
+            <p className="createdBy">Created by Benito Juarez, Tara Meazell, Matvey Lubaev and Rayyan Khan </p>
         </>
     )
 }
